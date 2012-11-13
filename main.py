1ec82958f5d9de69b437bdf4e5fd2130694aac56
Vitaly <linux.russia@gmail.com>
2012-11-13 16:07:50 +0400

    creation of three processes (not tested)

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-#TODO: 1. Главный процесс генерирует списки целых чисел
+#TODO: 1. Первый процесс генерирует списки целых чисел
 #TODO: 2. ..и отдает их второму процессу, который сортирует их
 #TODO: 3. ..и отдает 3-му процессу, который выкидывает из них простые числа и выводит на экран
 
 import random
-from multiprocessing import Queue
+from multiprocessing import Process, Queue
+
+def func_one(queue_out):
+    p, q, m, n = 10, 100, 5, 10
+    for i in xrange(m):
+        lst = list()
+        for j in xrange(n):
+            lst.append(random.randint(p, q))
+        queue_out.put(lst)
+
+def func_two(queue_in, queue_out):
+    while not queue_in.empty():
+        lst = queue_in.get()
+        lst.sort()
+        queue_out.put(lst)
+
+def func_three(queue_in):
+    while not queue_in.empty():
+        lst = queue_in.get()
+        print lst
 
 def main():
     queue_one = Queue()
     queue_two = Queue()
 
-    # генерация m списков по n целых элементов в диапазоне (p, q)
-    p = 10
-    q = 100
-    m = 5
-    n = 10
-    for i in xrange(m):
-        temp_list = list()
-        for j in xrange(n):
-            temp_list.append(random.randint(p, q))
-        print temp_list
-        queue_one.put(temp_list)
+    proc_one = Process(target=func_one, args=(queue_one))
+    proc_one.daemon = True
+    proc_one.start()
+
+    proc_two = Process(target=func_two, args=(queue_one, queue_two))
+    proc_two.daemon = True
+    proc_two.start()
+
+    proc_three = Process(target=func_three, args=(queue_two))
+    proc_three.daemon = True
+    proc_three.start()
+
+    proc_one.join()
+    proc_two.join()
+    proc_three.join()
 
 if __name__ == '__main__':
     main()
